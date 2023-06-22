e1aaab25432262a6e9feac77674ef3ba4794a34d
zzz332choi <123616196+zzz332choi@users.noreply.github.com>
2023-06-22 23:28:22 +0900

    Add files via upload

---
+++ 11651.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+struct pos {
+	int xpos, ypos;
+};
+
+int static compare(const void* first, const void* second) {
+	if ((*(pos*)first).ypos > (*(pos*)second).ypos) return 1;
+	else if ((*(pos*)first).ypos < (*(pos*)second).ypos) return -1;
+	else {
+		if ((*(pos*)first).xpos > (*(pos*)second).xpos) return 1;
+		else if ((*(pos*)first).xpos < (*(pos*)second).xpos) return -1;
+		else return 0;
+	}
+}
+
+int main() {
+
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int n;
+	cin >> n;
+
+	pos* P = new pos[n];
+
+	for (int i = 0; i < n; i++) cin >> P[i].xpos >> P[i].ypos;
+
+	qsort(P, n, sizeof(P), compare);
+
+	for (int i = 0; i < n; i++) cout << P[i].xpos << ' ' << P[i].ypos << '\n';
+
+	delete[] P;
+}
