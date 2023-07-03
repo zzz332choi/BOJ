8666ed3e279ddb1515c342a0eedb7089f249ba6f
zzz332choi <123616196+zzz332choi@users.noreply.github.com>
2023-07-03 14:39:54 +0900

    Create 10569.cpp

---
+++ 10569.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		int v, e;
+		cin >> v >> e;
+
+		cout << 2 - v + e << endl;
+	}
+
+}
