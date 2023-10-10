9d1aaa4c2b9c50f0c130c5d0c3c01e05b071049e
zzz332choi <123616196+zzz332choi@users.noreply.github.com>
2023-10-10 14:48:15 +0900

    Create 2010.cpp

---
+++ 2010.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+
+	int sum = 0;
+
+	for (int i = 0; i < n; i++) {
+		int num;
+		cin >> num;
+
+		sum += num;
+	}
+
+	cout << sum - n + 1;
+}
