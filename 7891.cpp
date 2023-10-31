ab93389f674baed5ea809367fa55e8447a684c19
zzz332choi <123616196+zzz332choi@users.noreply.github.com>
2023-10-31 13:47:59 +0900

    Create 7891.cpp

---
+++ 7891.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+#define endl '\n'
+
+int main() {
+	int n;
+	cin >> n;
+
+	while (n--) {
+		int e1, e2;
+		cin >> e1 >> e2;
+
+		cout << e1 + e2 << endl;
+	}
+}
